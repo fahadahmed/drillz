8bbca0c556d59fa05d64614820a160fb70cd8e6a
Fahad Ahmed <fahad.ahmed@me.com>
2021-12-08 21:13:44 +1100

    DRIL-6 created a sample app component to check husky

---
+++ packages/app/src/pages/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return(
+    <div>App Component</div>
+  )
+}
+
+export default App;
--- packages/app/src/pages/routes.tsx
+++ packages/app/src/pages/routes.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import App from './App';
 
 function Routes() {
   console.log(
     process.env.DRILLZ_APP_API_KEY,
     process.env.DRILLZ_APP_AUTH_DOMAIN,
   );
-  return <div>This is the React App.</div>;
+  return <App />;
 }
 
 export default Routes;
