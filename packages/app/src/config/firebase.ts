57bf58907d67048df183c11709389aecffb15c10
Fahad Ahmed <fahad.ahmed@me.com>
2021-12-20 15:42:49 +1100

    DRIL-6 fixing indent rule in eslint

--- packages/app/src/config/firebase.ts
+++ packages/app/src/config/firebase.ts
@@ -1,26 +1,26 @@
 import { initializeApp } from 'firebase/app';
 import {
-	getFirestore,
-	collection,
-	getDocs,
-	connectFirestoreEmulator,
+  getFirestore,
+  collection,
+  getDocs,
+  connectFirestoreEmulator,
 } from 'firebase/firestore/lite';
 import {
-	getAuth,
-	createUserWithEmailAndPassword,
-	signInWithEmailAndPassword,
-	connectAuthEmulator,
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  connectAuthEmulator,
 } from 'firebase/auth';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-	apiKey: process.env.DRILLZ_APP_API_KEY,
-	authDomain: process.env.DRILLZ_APP_AUTH_DOMAIN,
-	projectId: process.env.DRILLZ_APP_PROJECT_ID,
-	storageBucket: process.env.DRILLZ_APP_STORAGE_BUCKET,
-	messengerSenderId: process.env.DRILLZ_APP_MESSENGER_SENDER_ID,
-	appId: process.env.DRILLZ_APP_APP_ID,
-	measurementId: process.env.DRILLZ_APP_MEASUREMENT_ID,
+  apiKey: process.env.DRILLZ_APP_API_KEY,
+  authDomain: process.env.DRILLZ_APP_AUTH_DOMAIN,
+  projectId: process.env.DRILLZ_APP_PROJECT_ID,
+  storageBucket: process.env.DRILLZ_APP_STORAGE_BUCKET,
+  messengerSenderId: process.env.DRILLZ_APP_MESSENGER_SENDER_ID,
+  appId: process.env.DRILLZ_APP_APP_ID,
+  measurementId: process.env.DRILLZ_APP_MEASUREMENT_ID,
 };
 
 const fire = initializeApp(firebaseConfig);
@@ -33,11 +33,11 @@ connectAuthEmulator(auth, 'http://locahost:9099');
 
 export default fire;
 export {
-	db,
-	auth,
-	collection,
-	getDocs,
-	createUserWithEmailAndPassword,
-	signInWithEmailAndPassword,
-	analytics,
+  db,
+  auth,
+  collection,
+  getDocs,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  analytics,
 };
--- packages/app/src/pages/routes.tsx
+++ packages/app/src/pages/routes.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import {
-	BrowserRouter as Router,
-	Route,
-	Routes,
+  BrowserRouter as Router,
+  Route,
+  Routes,
 } from 'react-router-dom';
 import App from './App';
 
 function AppRoutes() {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<App />} />
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default AppRoutes;
