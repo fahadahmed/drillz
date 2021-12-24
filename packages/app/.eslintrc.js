1eaa66e81d932aa4d8542406bc60578176d2e308
Fahad Ahmed <fahad.ahmed@me.com>
2021-12-24 11:39:34 +1100

    DRIL-6 updated lint-staged command

--- packages/app/.eslintrc.js
+++ packages/app/.eslintrc.js
@@ -1,33 +1,53 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'eslint-config-prettier'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+        amd: true,
     },
-    ecmaVersion: 13,
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  }
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+        'plugin:jsx-a11y/strict',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['react', 'jsx-a11y', '@typescript-eslint'],
+    rules: {
+        'react-hooks/exhaustive-deps': 'error',
+        'no-var': 'error',
+        'brace-style': 'error',
+        'prefer-template': 'error',
+        radix: 'error',
+        'space-before-blocks': 'error',
+        'import/prefer-default-export': 'off',
+    },
+    overrides: [
+        {
+            files: [
+                '**/*.test.js',
+                '**/*.test.jsx',
+                '**/*.test.tsx',
+                '**/*.spec.js',
+                '**/*.spec.jsx',
+                '**/*.spec.tsx',
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
+    globals: {
+        window: true,
+        module: true,
+        process: true,
+    },
 };
