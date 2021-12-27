d49c8de4334e794c6ccae90120fc754bf912b935
Fahad Ahmed <fahad.ahmed@me.com>
2021-12-27 19:57:53 +1100

    DRIL-10 created a new page component - Register

--- packages/app/src/pages/routes.tsx
+++ packages/app/src/pages/routes.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
+import { Register } from '.';
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/sign-up" element={<Register />} />
       </Routes>
     </Router>
   );
