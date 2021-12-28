c86a549be18a2d4426adeb2a540cbb2d7a5d8a80
Fahad Ahmed <fahad.ahmed@me.com>
2021-12-28 17:40:04 +1100

    DRIL-10 created a form in the register page

--- packages/app/src/pages/Register/Register.tsx
+++ packages/app/src/pages/Register/Register.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TextInput = styled.input`
+  border: 1px solid #000;
+  padding: 12px;
+  color: #000000;
+  font-size: 20px;
+  margin: 12px;
+`;
+
+const Button = styled.button`
+  border: none;
+  background: #000;
+  padding: 12px;
+  text-decoration: none;
+  color: #fff;
+  font-weight: bold;
+  margin: 12px;
+  &:hover {
+    background: #fff;
+    color: #000;
+  }
+`;
 
 function Register() {
   return (
     <div>
-      <h1>Register Page</h1>
+      <h1>
+        Drillz App <small>Register</small>
+      </h1>
+      <Container>
+        <TextInput type="text" placeholder="Enter your name" />
+        <TextInput type="text" placeholder="Enter your email" />
+        <TextInput type="text" placeholder="Enter a password" />
+        <Button>Create User</Button>
+      </Container>
     </div>
   );
 }
