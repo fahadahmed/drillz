94eb9cae44f54e11612930eff22550f4ef26d8a4
Fahad Ahmed <fahad.ahmed@me.com>
2021-12-28 16:51:36 +1100

    DRIL-10 added the AppContainer to be used in the app

--- packages/app/src/pages/routes.tsx
+++ packages/app/src/pages/routes.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import { Register } from '.';
+import { AppContainer } from '../components';
 
 function AppRoutes() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/sign-up" element={<Register />} />
-      </Routes>
-    </Router>
+    <AppContainer>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/sign-up" element={<Register />} />
+        </Routes>
+      </Router>
+    </AppContainer>
   );
 }
 
