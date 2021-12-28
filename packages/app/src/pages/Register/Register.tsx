f0b2936dacb669857997b9596cc930cd499441d7
Fahad Ahmed <fahad.ahmed@me.com>
2021-12-28 22:17:02 +1100

    DRIL-10 implemented creating user in firebase

--- packages/app/src/pages/Register/Register.tsx
+++ packages/app/src/pages/Register/Register.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState, useContext, SyntheticEvent } from 'react';
 import styled from '@emotion/styled';
+import { Navigate } from 'react-router-dom';
+import { AppContext } from '../../components';
+import { auth, createUserWithEmailAndPassword } from '../../config/firebase';
 
-const Container = styled.div`
+const Form = styled.form`
   display: flex;
   flex-direction: column;
 `;
@@ -29,17 +32,46 @@ const Button = styled.button`
 `;
 
 function Register() {
+  const [error, setError] = useState(null);
+  const { currentUser } = useContext(AppContext);
+
+  if (currentUser) {
+    return <Navigate to="/dashboard" />;
+  }
+
+  const handleSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      name: { value: string };
+      email: { value: string };
+      password: { value: string };
+    };
+    // const name = target.name.value;
+    const email = target.email.value;
+    const password = target.password.value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(err.message);
+      });
+  };
+
   return (
     <div>
       <h1>
         Drillz App <small>Register</small>
       </h1>
-      <Container>
-        <TextInput type="text" placeholder="Enter your name" />
-        <TextInput type="text" placeholder="Enter your email" />
-        <TextInput type="text" placeholder="Enter a password" />
-        <Button>Create User</Button>
-      </Container>
+      <Form onSubmit={handleSubmit}>
+        <TextInput type="text" name="name" placeholder="Enter your name" />
+        <TextInput type="text" name="email" placeholder="Enter your email" />
+        <TextInput type="password" name="password" placeholder="Enter a password" />
+        <Button type="submit">Create User</Button>
+        {error && <div>{error}</div>}
+      </Form>
     </div>
   );
 }
--- packages/app/src/pages/routes.tsx
+++ packages/app/src/pages/routes.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import { Register } from '.';
-import { AppContainer } from '../components';
+import { AppContainer, AppProvider } from '../components';
 
 function AppRoutes() {
   return (
-    <AppContainer>
-      <Router>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/sign-up" element={<Register />} />
-        </Routes>
-      </Router>
-    </AppContainer>
+    <AppProvider>
+      <AppContainer>
+        <Router>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/sign-up" element={<Register />} />
+          </Routes>
+        </Router>
+      </AppContainer>
+    </AppProvider>
   );
 }
 
