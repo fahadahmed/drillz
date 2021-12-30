d206bbd0a55ac7631a874bc2edcda98c24f19d6c
Fahad Ahmed <fahad.ahmed@me.com>
2021-12-30 11:36:24 +1100

    DRIL-10 created Private and Public routes using react-router v6

--- packages/app/src/pages/routes.tsx
+++ packages/app/src/pages/routes.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './App';
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { Register, Dashboard } from '.';
-import { AppContainer, AppProvider } from '../components';
+import { AppContainer, AppContext } from '../components';
 
 function AppRoutes() {
+  const { currentUser } = useContext(AppContext);
   return (
-    <AppProvider>
-      <AppContainer>
-        <Router>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/sign-up" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </Router>
-      </AppContainer>
-    </AppProvider>
+    <AppContainer>
+      <Router>
+        <Routes>
+          {!currentUser && (
+            <>
+              <Route path="/sign-up" element={<Register />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/" element={<Navigate to="/sign-in" />} />
+            </>
+          )}
+          {currentUser && <Route path="/dashboard" element={<Dashboard />} />}
+          <Route path="*" element={<Navigate to={currentUser ? '/dashboard' : '/sign-in'} />} />
+        </Routes>
+      </Router>
+    </AppContainer>
   );
 }
 
 export default AppRoutes;
+
+const Login = () => <div>Login Page</div>;
