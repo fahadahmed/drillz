3112dd5ce2a2b5f9f7051e530ed072fa444078e3
Fahad Ahmed <fahad.ahmed@me.com>
2022-01-01 13:29:26 +1100

    DRIL-12 removed the logout button from the dashboard page

--- packages/app/src/pages/Dashboard/Dashboard.tsx
+++ packages/app/src/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { auth } from '../../config/firebase';
 
 function Dashboard() {
   return (
     <div>
       <h1>Dashboard</h1>
-      <button type="button" onClick={() => auth.signOut()}>
-        Log Out
-      </button>
     </div>
   );
 }
