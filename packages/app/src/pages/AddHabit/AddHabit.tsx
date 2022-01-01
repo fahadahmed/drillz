62a263a417a18f47f6d4669e8383cb84751dd791
Fahad Ahmed <fahad.ahmed@me.com>
2022-01-01 20:25:48 +1100

    DRIL-12 created GoodHabit component

--- packages/app/src/pages/AddHabit/AddHabit.tsx
+++ packages/app/src/pages/AddHabit/AddHabit.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+import { GoodHabit } from './components';
+
+const PageContainer = styled.div`
+  padding: 16px;
+`;
 
 function AddHabit() {
+  const [habitType, setHabitType] = useState(null);
+
+  const handleHabitType = (habit: string) => {
+    setHabitType(habit);
+  };
+
   return (
-    <div>
-      <h1>Add New Habit</h1>
-    </div>
+    <PageContainer>
+      <h2>Add New Habit</h2>
+      <div>
+        <button onClick={() => handleHabitType('createHabit')}>Create Good Habit</button>
+        <button onClick={() => handleHabitType('breakHabit')}>Break Bad Habit</button>
+      </div>
+      <div>
+        {habitType === 'createHabit' && <GoodHabit />}
+        {habitType === 'breakHabit' && (
+          <div>
+            <h3>Break Bad Habit</h3>
+          </div>
+        )}
+      </div>
+    </PageContainer>
   );
 }
 
---
+++ packages/app/src/pages/AddHabit/components/index.ts
@@ -0,0 +1,1 @@
+export { default as GoodHabit } from './GoodHabit';
