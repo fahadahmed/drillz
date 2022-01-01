7139450698e2d058ca75d9ac5f131ebe8e8f6120
Fahad Ahmed <fahad.ahmed@me.com>
2022-01-01 14:25:36 +1100

    DRIL-12 created a custom page layout for the dashboard page

--- packages/app/src/pages/Dashboard/Dashboard.tsx
+++ packages/app/src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  height: 100vh;
+`;
+
+const Pane = styled.div`
+  display: grid;
+  grid-template-rows: 80px 1fr;
+  border: 2px solid #999;
+`;
+const PaneHeader = styled.div`
+  border-bottom: 2px solid #999;
+  padding: 16px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const PaneContent = styled.div`
+  padding: 16px;
+`;
 
 function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <Container>
+      <Pane>
+        <PaneHeader>
+          <h2>All Habits</h2>
+          <button>Add Habit</button>
+        </PaneHeader>
+        <PaneContent>
+          <p>This is the content section of the pane</p>
+        </PaneContent>
+      </Pane>
+      <Pane>
+        <PaneHeader>
+          <h2>Habit Analysis</h2>
+        </PaneHeader>
+        <PaneContent>
+          <p>This is the content section of the pane</p>
+        </PaneContent>
+      </Pane>
+    </Container>
   );
 }
 
