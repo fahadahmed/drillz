526aa4a3740a3c7edd7e965a17c655e25aa10b05
Fahad Ahmed <fahad.ahmed@me.com>
2022-01-02 20:40:58 +1100

    DRIL-12 added code to show a list of habits on the dashboard

--- packages/app/src/pages/Dashboard/components/ShowHabits/ShowHabits.tsx
+++ packages/app/src/pages/Dashboard/components/ShowHabits/ShowHabits.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { AppContext } from '../../../../components';
+import { db, doc, getDoc } from '../../../../config/firebase';
+
+interface Habit {
+  habitName: string;
+  goal: number;
+  unit: string;
+  timeframe: string;
+}
 
 function ShowHabits() {
+  const [habits, setHabits] = useState(null);
+  const { currentUser } = useContext(AppContext);
+
+  const retrieveUserDetails = async () => {
+    const userData = await getDoc(doc(db, `userData/${currentUser.uid}`));
+    setHabits(userData.data().habits);
+  };
+
+  useEffect(() => {
+    retrieveUserDetails();
+
+    return () => {
+      setHabits(null);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div>
-      <p>Here we should show the habits</p>
+      {habits && habits.map((habit: Habit, i: number) => <div key={i}>{habit.habitName}</div>)}
     </div>
   );
 }
